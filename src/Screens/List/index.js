be3b95b8e154f7f365c54d8153517a36863e9626
artem <artem98.tema@gmail.com>
2023-03-01 22:03:41 +0300

    Добавил отображение посуды

--- src/Screens/List/index.js
+++ src/Screens/List/index.js
@@ -37,8 +37,10 @@ const tea=[
     {name:'красный чай'},
     {name:'белый чай',},
     {name:'пуэр чай',}]
-const window=[{name:'окно 1',id:1}, {name:'окно 2',id:2}, {name:'еще окно',id:3}]
-
+const dishesTea=[
+    {name:'чайник глиняный',img:'P1',text:'й. '},
+    {name:'гайвань',img:'P2',text:'-'},
+    {name:'чайник стеклянный',img:'P3',text:'as'},]
 const Item = ({title}) => (
     <View style={styles.item}>
         <Text style={styles.title}>{title}</Text>
@@ -48,16 +50,26 @@ const List = () => {
     const navigation = useNavigation();
     const route = useRoute();
     const goToList=(item)=>{
+        if(item.text){
+            navigation.navigate('Detail',{
+                name: item.name,
+                img:item.img,
+                text:item.text
+            })
+            return
+        }
         navigation.navigate('ListDetail',{
             name: item.name,
             img:item.img
         })
     }
 
+
+
     return(
         <View style={styles.container}>
             <FlatList
-                data={route.params?.name==='Чай'?tea:window}
+                data={route.params?.name==='Чай'?tea:dishesTea}
                 renderItem={({item}) => <TouchableOpacity onPress={()=>goToList(item)}><Item title={item.name} /></TouchableOpacity>}
                 keyExtractor={item => item.img}
             />
